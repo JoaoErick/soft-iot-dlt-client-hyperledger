22972712299933171984d66959db2eee0bf96518
JoaoErick <jerick1415@outlook.com>
2023-10-17 12:25:41 -0300

    chore: receiving request json from top-k

--- src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/ListenerDeviceRequest.java
+++ src/main/java/br/uefs/larsid/dlt/iot/soft/mqtt/ListenerDeviceRequest.java
@@ -4,6 +4,9 @@ import br.uefs.larsid.dlt.iot.soft.services.Controller;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -13,20 +16,21 @@ public class ListenerDeviceRequest implements IMqttMessageListener {
   /*-------------------------Constants---------------------------------------*/
   private static final String AUTHENTICATED_DEVICES = "AUTHENTICATED_DEVICES";
   private static final String AUTHENTICATED_DEVICES_RES = "AUTHENTICATED_DEVICES_RES";
+
+  private static final int TIMEOUT_IN_SECONDS = 30;
   
   private static final int QOS = 1;
   /*-------------------------------------------------------------------------*/
 
   private boolean debugModeValue;
-  private MQTTClient MQTTClientUp;
   private MQTTClient MQTTClientHost;
   private Controller controllerImpl;
+  private String nodeUri;
   private static final Logger logger = Logger.getLogger(ListenerDeviceRequest.class.getName());
   /**
    * Builder method.
    *
    * @param controllerImpl Controller - Controller that will make use of this Listener.
-   * @param MQTTClientUp   MQTTClient - Upper Gateway MQTT Client.
    * @param MQTTClientHost MQTTClient - Gateway's own MQTT client.
    * @param topics         String[] - Topics to be subscribed to.
    * @param qos            int - Quality of service of the topic that will be heard.
@@ -34,16 +38,16 @@ public class ListenerDeviceRequest implements IMqttMessageListener {
    */
   public ListenerDeviceRequest(
     Controller controllerImpl,
-    MQTTClient MQTTClientUp,
     MQTTClient MQTTClientHost,
+    String nodeUri,
     String[] topics,
     int qos,
     boolean debugModeValue
   ) {
-    this.MQTTClientUp = MQTTClientUp;
     this.MQTTClientHost = MQTTClientHost;
     this.controllerImpl = controllerImpl;
     this.debugModeValue = debugModeValue;
+    this.nodeUri = nodeUri;
 
     for (String topic : topics) {
         this.MQTTClientHost.subscribe(qos, this, topic);
@@ -53,24 +57,77 @@ public class ListenerDeviceRequest implements IMqttMessageListener {
   @Override
   public void messageArrived(String topic, MqttMessage message)
     throws Exception {
+    final String mqttMessage = new String(message.getPayload());
 
     switch (topic) {
         case AUTHENTICATED_DEVICES:
-
-          printlnDebug("Requesting proof of credential for connected devices...");
+          printlnDebug("==== Fog gateway -> Bottom gateway  ====");
+          printlnDebug("(Hyperledger Bundle) Requesting proof of credential for connected devices...\n");
 
           List<String> deviceIdsAuths = new ArrayList<>();
+          JsonObject jsonGetTopKDown = new Gson()
+            .fromJson(mqttMessage, JsonObject.class);
 
-          /* Send request for presentation */
-          for (Map.Entry<String, String> connectionIdDeviceNode : this.controllerImpl.getConnectionIdDeviceNodes().entrySet()) {
-            String deviceId = connectionIdDeviceNode.getKey();
-            printlnDebug("Device ID: " + deviceId);
-            this.controllerImpl.sendRequestPresentationRequest(connectionIdDeviceNode.getValue());
-            deviceIdsAuths.add(deviceId);
+          try {
+            /* Send request for presentation */
+            for (Map.Entry<String, String> connectionIdDeviceNode : this.controllerImpl.getConnectionIdDeviceNodes().entrySet()) {
+              String deviceId = connectionIdDeviceNode.getKey();
+
+              printlnDebug("------------- Proof of Credential -------------");
+              printlnDebug("Device ID: " + deviceId);
+              printlnDebug("-----------------------------------------------\n");
+
+              this.controllerImpl.sendRequestPresentationRequest(connectionIdDeviceNode.getValue());
+
+              long start = System.currentTimeMillis();
+              long end = start + TIMEOUT_IN_SECONDS * 1000;
+
+              /*
+                * Enquanto o dispositivo ainda não provou que possui uma 
+                * credencial verificável e o tempo configurado não terminou.
+                */
+              while (
+                !this.controllerImpl.isProofOfCredentialReceived() &&
+                System.currentTimeMillis() < end
+              ) {}
+
+              this.controllerImpl.setProofOfCredentialReceived(false);
+
+              deviceIdsAuths.add(deviceId);
+            }
+          } catch (Exception e) {
+            printlnDebug("!Error when making proof requests for devices!");
           }
 
-          printlnDebug("Autheticated Devices: " + deviceIdsAuths);
-          MQTTClientHost.publish(AUTHENTICATED_DEVICES_RES, deviceIdsAuths.toString().getBytes(), QOS);
+
+          printlnDebug(
+            "(Hyperledger Bundle) Autheticated Devices = " + 
+            deviceIdsAuths + 
+            "\n"
+          );
+
+          try {
+            jsonGetTopKDown.addProperty("authDevices", deviceIdsAuths.toString());
+            this.MQTTClientHost.publish(AUTHENTICATED_DEVICES_RES, jsonGetTopKDown.toString().getBytes(), QOS);
+          } catch (Exception e) {
+            printlnDebug("!Error when publishing to send authenticated devices!");
+
+            String user = "karaf";
+            String password = "karaf";
+            String uri[] = this.nodeUri.split(":");
+
+            MQTTClient MQTTClient= new MQTTClient(
+              this.debugModeValue,
+              uri[0],
+              uri[1],
+              user,
+              password
+            );
+
+            MQTTClient.connect();
+            MQTTClient.publish(AUTHENTICATED_DEVICES_RES, deviceIdsAuths.toString().getBytes(), QOS);
+            //MQTTClient.disconnect();
+          }
 
           break;
         default:
